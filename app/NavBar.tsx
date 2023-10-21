e8e36d9ef96f9820aaebc1ca9c348dc73d597552
Nabil Affo <nabil.noble01@gmail.com>
2023-10-21 11:47:40 +0000

    style navbar active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,17 @@
+'use client';
+
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  // console.log(currentPath);
+
   const links = [
     { label: 'Dashboard', href: '/'},
-    { label: 'Issues', href: '/'},
+    { label: 'Issues', href: '/issues'},
     // { label: 'Dashboard', href: '/'},
     // { label: 'Dashboard', href: '/'},
     // { label: 'Dashboard', href: '/'},
@@ -18,7 +25,12 @@ const NavBar = () => {
         {links.map(link =>
           <Link
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              'text-zinc-900': link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true,
+            })}
+            // className={`${link.href === currentPath ? "text-zinc-900" : "text-zinc-500"} hover:text-zinc-800 transition-colors`}
             href={link.href}
           >
             {link.label}
