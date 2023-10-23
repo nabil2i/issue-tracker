71e3f697e13ef268d6026ef8a1264bee63d298da
Nabil Affo <nabil.noble01@gmail.com>
2023-10-23 08:29:46 +0000

    add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Callout, Text, TextField, } from "@radix-ui/themes";
@@ -22,6 +23,7 @@ const NewIssuePage = () => {
   });
   const router = useRouter();
   const [ error, setError ] = useState('');
+  const [ isSubmitting, setSubimitting ] = useState(false);
 
   return (
     <div className="max-w-xl">
@@ -34,6 +36,7 @@ const NewIssuePage = () => {
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            setSubimitting(true);
             await axios.post('/api/issues', data);
             router.push('/issues')
           } catch (error) {
@@ -55,7 +58,7 @@ const NewIssuePage = () => {
             )}
             />
           <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner/>}</Button>
       </form>
     </div>
   );
