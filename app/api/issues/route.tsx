0e4f226d72f3edb353d48c51bda35bedace501a4
Nabil Affo <nabil.noble01@gmail.com>
2023-10-23 19:24:16 +0000

    secure the api

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,8 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import {  issueSchema as createIssueSchema } from "../../validationSchemas";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session)
+    return NextResponse.json({ error: "User not authorized"}, { status: 401 });
+
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
 
