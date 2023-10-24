f1292fd66651d9c0495097c89e4ff6d0c6eaa520
Nabil Affo <nabil.noble01@gmail.com>
2023-10-24 11:34:55 +0000

    build issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import { Box } from "@radix-ui/themes";
 import Pagination from "./components/Pagination";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" }});
+  const inProgress = await prisma.issue.count({ where: { status: "IN_PROGRESS" }});
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" }});
+
   return (
   <Box>
     <LatestIssues />
+    <IssueSummary open={open} inProgress={inProgress} closed={closed}/>
   </Box>
   );
 }
