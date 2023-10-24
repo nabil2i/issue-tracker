489baffe1fbb67aecb287409d2a403b39f77dec6
Nabil Affo <nabil.noble01@gmail.com>
2023-10-24 09:18:29 +0000

    filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,10 +1,24 @@
 import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
+import { Status } from "@prisma/client";
 import { Table } from "@radix-ui/themes";
 import IssueActions from "./IssueActions";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status)
+  // console.log(statuses)
+  // console.log(searchParams.status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+    
+  const issues = await prisma.issue.findMany({
+    where: { status }
+  });
   // await delay(2000);
 
   return (
