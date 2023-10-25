c8dd416a5a2279fd9e05de8a09f44c8f9ac51333
Nabil Affo <nabil.noble01@gmail.com>
2023-10-25 19:05:48 +0000

    add description box to IssueStatusFilter

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Status } from "@prisma/client";
-import { Select } from "@radix-ui/themes";
+import { Select, Flex, Text } from "@radix-ui/themes";
 import { useRouter, useSearchParams } from "next/navigation";
 
 const statuses: { label: string; value?: Status }[] = [
@@ -16,27 +16,30 @@ const IssueStatusFilter = () => {
   const searchParams = useSearchParams();
 
   return (
-    <Select.Root
-      defaultValue={searchParams.get('status') || ''}
-      onValueChange={(status) => {
-        const params = new URLSearchParams();
-        if (status) params.append("status", status);
-        if (searchParams.get("orderBy"))
-          params.append("orderBy", searchParams.get("orderBy")!);
+    <Flex align="center" gap="2">
+      <Text>Filter by status: </Text>
+      <Select.Root
+        defaultValue={searchParams.get('status') || ''}
+        onValueChange={(status) => {
+          const params = new URLSearchParams();
+          if (status) params.append("status", status);
+          if (searchParams.get("orderBy"))
+            params.append("orderBy", searchParams.get("orderBy")!);
 
-        const query = params.size ? '?' + params.toString() : ''; 
-        router.push("/issues/list" + query);
-      }}
-    >
-      <Select.Trigger placeholder="Filter by status..." />
-      <Select.Content>
-        {statuses.map((status) => (
-          <Select.Item key={status.value || "all"} value={status.value || ""}>
-            {status.label}
-          </Select.Item>
-        ))}
-      </Select.Content>
-    </Select.Root>
+          const query = params.size ? '?' + params.toString() : ''; 
+          router.push("/issues/list" + query);
+        }}
+      >
+        <Select.Trigger placeholder="Filter by status..." />
+        <Select.Content>
+          {statuses.map((status) => (
+            <Select.Item key={status.value || "all"} value={status.value || ""}>
+              {status.label}
+            </Select.Item>
+          ))}
+        </Select.Content>
+      </Select.Root>
+    </Flex>
   );
 };
 
