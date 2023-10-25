e48918189b56d969b8c212722a5b569bd23e70f9
Nabil Affo <nabil.noble01@gmail.com>
2023-10-25 19:58:03 +0000

    implement change status

---
+++ app/issues/[id]/StatusSelect.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Skeleton } from "@/app/components";
+import { Status, Issue } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast, { Toaster } from "react-hot-toast";
+
+const statuses: { label: string; value?: Status }[] = [
+  { label: "Open", value: "OPEN" },
+  { label: "In Progress", value: "IN_PROGRESS" },
+  { label: "Closed", value: "CLOSED" },
+];
+
+const StatusSelect = ({ issue }: { issue: Issue }) => {
+  const router = useRouter();
+ 
+  const changeStatus = (status: Status) => {
+    // console.log(status)
+    axios
+      .patch("/api/issues/" + issue.id, { status })
+      .then(() => {
+        toast.success("Status changed.");
+        router.push("/issues/" + issue.id);
+        router.refresh();
+      })
+      .catch(() => {
+        toast.error("Changes could not be saved.");
+      });
+  }
+
+  return (
+    <>
+      <Select.Root
+        defaultValue={issue.status}
+        onValueChange={changeStatus}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Change Issue Status</Select.Label>
+            {/* <Select.Item value="">Unassigned</Select.Item> */}
+            {statuses?.map((status) => (
+              <Select.Item key={status.value} value={status.value || ""}>
+                {status.label}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
+  )
+}
+
+export default StatusSelect
