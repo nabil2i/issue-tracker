76366a870facfe0e4c7d43f023effee0b31e5758
Nabil Affo <nabil.noble01@gmail.com>
2023-10-25 20:42:33 +0000

    update status to in progress when use is assigned

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -3,12 +3,14 @@
 import { Skeleton } from "@/app/components";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const { data: users, error, isLoading } = useUsers();
+  const router = useRouter();
 
   if (isLoading) return <Skeleton />;
 
@@ -24,9 +26,15 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   // }, []);
 
   const assignIssue = (userId: string) => {
+    const updatedStatus = userId ? "IN_PROGRESS" : "OPEN";
     axios
       .patch("/api/issues/" + issue.id, {
         assignedToUserId: userId || null,
+        status: updatedStatus,
+      })
+      .then(() => {
+        router.refresh()
+        toast.success("User assigned.");
       })
       .catch(() => {
         toast.error("Changes could not be saved.");
